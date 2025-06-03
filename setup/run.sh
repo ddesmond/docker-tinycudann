4aed8819005260b0e4c36014c862d128d05f0222
Aleks <crni.aki@gmail.com>
2025-06-03 08:39:35 +0200

    update

--- setup/run.sh
+++ setup/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "_____ Startup _____"
-if [ -e /setup/.setup-init ]; then
-  echo "copy files to data"
-  mkdir /data/dist && chmod -R 777 /data/dist
-  cp -f /opt/dist /data/dist
-else
-  rm -rf /setup/.setup-init
-fi
+#echo "_____ Startup _____"
+#if [ -e /setup/.setup-init ]; then
+#  echo "copy files to data"
+#  mkdir /data/dist && chmod -R 777 /data/dist
+#  cp -f /opt/dist /data/dist
+#else
+#  rm -rf /setup/.setup-init
+#fi
 sleep infinity
