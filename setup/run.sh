29b570113af95930cbc7f9ffad15d28c3aa36459
Aleks <crni.aki@gmail.com>
2025-06-03 12:07:26 +0200

    update

--- setup/run.sh
+++ setup/run.sh
@@ -3,7 +3,7 @@ echo "_____ Startup _____"
 if [ -e /opt/dist/tinycudann-1.7-py3.10.egg-info ]; then
   echo "copy files to data"
   mkdir /data/dist && chmod -R 777 /data/dist
-  cp -f /opt/dist /data/dist
+  cp -rf /opt/dist /data/dist
 else
   rm -rf /setup/.setup-init
 fi
